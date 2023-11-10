982143d3a2d7dc516b0a195f0c417eb5a8213b73
AlexanderQn <146363969+AlexanderQn@users.noreply.github.com>
2023-11-10 23:44:13 +0000

    Database fused with main Menue
    few comments yet

---
+++ Database/Database/Main_Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Database
+{
+    public partial class Main_Menu : Form
+    {
+        public Main_Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //returns the object of the DB1Connection class 
+            DBConnection dbConn = DBConnection.getInstanceofDBConnection();
+            DataSet datasetProducts = dbConn.getDataSet("Select * FROM Products_Table");
+            dgv.DataSource = datasetProducts.Tables[0];
+        }
+    }
+}
