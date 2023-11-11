541d22c87aac522d278edd99bf2bf6b8eb5f5c54
AlexanderQn <146363969+AlexanderQn@users.noreply.github.com>
2023-11-11 18:12:38 +0000

    Main Programm
    Small adjustments
    more SQL
    more GUI work

---
+++ Database/Database/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    internal class Constants
+    {
+        public static string SHOWPREVIEWPRODUCTS = "SELECT Product_Name, Product_Type, Product_Description FROM Products_Table";
+        public static string SHOWPREVIEWVENDORS = "SELECT Company_Name, Company_Website, Company_Last_Review_Date FROM Companies_Table";
+        public static string Test = "SELECT Products_Table.Product_Name, Companies_Table.Company_Name FROM Companies_Table INNER JOIN Products_Table ON Companies_Table.Company_ID = Products_Table.Company_ID";
+        public static string Test1 = "SELECT Products_Table.Product_Name, Companies_Table.Company_Name FROM Companies_Table, Products_Table";
+    }
+}
---
+++ Database/Database/Main_Menu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Database
+{
+    public partial class Main_Menu : Form
+    {
+        public Main_Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //returns the object of the DBConnection class 
+            DBConnection dbConn = DBConnection.getInstanceofDBConnection();
+            DataSet datasetProducts = dbConn.getDataSet(Constants.SHOWPREVIEWPRODUCTS);
+            MainMenuedvg.DataSource = datasetProducts.Tables[0];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //returns the object of the DBConnection class 
+            DBConnection dbConn = DBConnection.getInstanceofDBConnection();
+            DataSet datasetProducts = dbConn.getDataSet(Constants.SHOWPREVIEWVENDORS);
+            MainMenuedvg.DataSource = datasetProducts.Tables[0];
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
